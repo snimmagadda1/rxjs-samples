e66e96851050604797b2f9a925c1cae8e38e452e
sai <sai.nimmagadda@optum.com>
2023-01-18 14:11:21 -0500

    add polling to display

--- 01-rxjs-hello-world/src/main.ts
+++ 01-rxjs-hello-world/src/main.ts
@@ -1,5 +1,5 @@
 import './style.css';
 import { timer } from 'rxjs';
 
-const timerElement: HTMLElement = document.querySelector<HTMLHeadElement>('#timer')!;
+const timerElement: HTMLElement = document.querySelector<HTMLElement>('#timer')!;
 timer(0, 1000).subscribe(t => timerElement.innerHTML = `Hello! It has been ${t} seconds`);
--- 08-http-polling/src/main.ts
+++ 08-http-polling/src/main.ts
@@ -1,21 +1,26 @@
 import './style.css';
 import { ajax } from 'rxjs/ajax';
-import { catchError, map, of, switchMap, take, timer } from 'rxjs';
+import { catchError, map, of, switchMap, take, tap, timer } from 'rxjs';
+let count = 0;
+const countElement = document.querySelector<HTMLElement>('#count');
 
-const getUsers$ = ajax('https://api.github.com/users?per_page=5').pipe(
-    map(userResponse => console.log('users: ', userResponse)),
-    catchError(error => {
-        console.log('error: ', error);
-        return of(error);
-    })
-);
-
-// Use timer as source to 'poll' the endpoint every 10 seconds
-const poll$ = timer(0, 10000).pipe(switchMap(() => getUsers$));
-
-// Addition of take() to set a maximum of 5 attempts
-poll$.pipe(take(5)).subscribe({
-    next: (value: any) => console.log(value),
-    error: (err: any) => console.log(err),
-    complete: () => console.log('completed poll')
-});
+if (countElement){
+    const getUsers$ = ajax('https://api.github.com/users?per_page=5').pipe(
+        map(userResponse => console.log('users: ', userResponse)),
+        catchError(error => {
+            console.log('error: ', error);
+            return of(error);
+        }),
+        tap((_) => countElement.innerHTML = `Made ${++count} requests`)
+    );
+    
+    // Use timer as source to 'poll' the endpoint every 10 seconds
+    const poll$ = timer(0, 10000).pipe(switchMap(() => getUsers$));
+    
+    // Addition of take() to set a maximum of 5 attempts
+    poll$.pipe(take(5)).subscribe({
+        next: (value: any) => console.log(value),
+        error: (err: any) => console.log(err),
+        complete: () => console.log('completed poll')
+    });
+}
